84bde186fa11a81f3ecfbaff41b46f1025bd3486
“lakshmikant” <fullstacktrainer4u@gmail.com>
2024-08-05 12:27:03 +0530

    primitive data types

---
+++ data_types/primitive.js
@@ -0,0 +1,22 @@
+const PI = 3.14
+console.log(typeof PI)
+var student1 = "Nidhi"
+var student2 = "Riya"
+var student3 = "Sriya"
+var student4 = "Serena"
+
+console.log(typeof student1)
+let attendingClass = true;
+console.log(typeof attendingClass)
+
+let projectName = null
+console.log(typeof projectName)
+
+let team;
+
+console.log(typeof team)
+
+let companyId = Symbol("everi")
+
+console.log(typeof companyId)
+
